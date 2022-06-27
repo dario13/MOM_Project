4121c34920000031926eb1f3e9b45627866bf029
dario <dariogabalec@gmail.com>
2022-06-26 21:17:03 -0300

    feat: created flexBox Component

---
+++ Frontend/src/components/atoms/flex-box/flex-box.props.ts
@@ -0,0 +1,29 @@
+export type FlexBoxProps = {
+  children?: any
+  className?: string
+  display?: 'flex' | 'inline-flex'
+  /** **** Container Props ********/
+  flexDirection?: 'row' | 'column' | 'row-reverse' | 'column-reverse'
+  justifyContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly'
+    | 'stretch'
+  flexWrap?: 'wrap' | 'nowrap' | 'wrap-reverse'
+  alignItems?: 'stretch' | 'center' | 'flex-start' | 'flex-end' | 'baseline'
+  /** **** Child Props ********/
+  flexGrow?: string
+  flexShrink?: string
+  flexBasis?: string
+  flex?: string
+  /** **** Common Layout Props ********/
+  padding?: string
+  margin?: string
+  width?: string
+  height?: string
+  maxWidth?: string
+  maxHeight?: string
+}
