9169d8799da919540522e8e3574c58981407341f
dario <dariogabalec@gmail.com>
2022-07-04 22:02:45 -0300

    test: fixed test

--- Frontend/src/components/atoms/text/text.test.tsx
+++ Frontend/src/components/atoms/text/text.test.tsx
@@ -1,29 +1,19 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { Text } from './text'
-import { componentColors, componentSizes } from '../../constants'
-import { TextAlignment } from './text.props'
+import { componentSizes } from '../../constants'
+import { textAlignments } from './text.props'
 
 describe('Text', () => {
   test('should render a Text component', () => {
     // Given
     const size = componentSizes[0]
-    const color = componentColors[0]
-    const align = TextAlignment[0]
+    const align = textAlignments[0]
     const exampleText = 'testing a text component'
     const bold = true
     const italic = true
 
-    render(
-      <Text
-        text={exampleText}
-        size={size}
-        color={color}
-        align={align}
-        bold={bold}
-        italic={italic}
-      />,
-    )
+    render(<Text text={exampleText} size={size} align={align} bold={bold} italic={italic} />)
 
     // When
     const renderedTextComponent = screen.getByTestId('Text')
@@ -32,7 +22,7 @@ describe('Text', () => {
     // Then
     expect(renderedTextComponent).toBeInTheDocument()
     expect(renderedTextComponent).toHaveTextContent(exampleText)
-    expect(renderedTextComponent).toHaveClass(`text-${size} text-${color} font-bold italic`)
+    expect(renderedTextComponent).toHaveClass(`text-${size} font-bold italic`)
     expect(alignTextElement).toHaveClass(`text-${align}`)
   })
 })
