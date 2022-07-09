bd9ed5d1a0c0dfa99b34e5e551f76c6ad9e73494
dario <dariogabalec@gmail.com>
2022-07-09 15:16:54 -0300

    fix: fixed constant name

--- Frontend/src/components/constants.ts
+++ Frontend/src/components/constants.ts
@@ -4,6 +4,6 @@ export const componentSizes = ['lg', 'md', 'sm', 'xs'] as const
 export const componentStatuses = ['info', 'success', 'warning', 'error'] as const
 export const brandColors = ['primary', 'secondary', 'accent'] as const
 export const componentColors = [...brandColors, 'ghost', ...componentStatuses] as const
-export const bgColors = ['base-100', 'base-200', 'base-300', 'neutral'] as const
+export const bgColors = ['base100', 'base200', 'base300', 'neutral'] as const
 
 export const defaultTheme = 'light'
