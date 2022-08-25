88eed7b132ea22b961073897d37d55b71c4f73a7
dario <dariogabalec@gmail.com>
2022-08-25 19:29:37 -0300

    chore: updated config

--- Backend/hardhat.config.ts
+++ Backend/hardhat.config.ts
@@ -1,7 +1,24 @@
 import { HardhatUserConfig } from 'hardhat/config'
+import '@typechain/hardhat'
+import 'hardhat-deploy'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import './tasks'
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
+  defaultNetwork: 'hardhat',
+  namedAccounts: {
+    deployer: 0,
+    tokenOwner: 1,
+    userA: 2,
+    userB: 3,
+    userC: 4,
+  },
+  typechain: {
+    outDir: 'typechain-types',
+    target: 'ethers-v5',
+  },
 }
 
 export default config
