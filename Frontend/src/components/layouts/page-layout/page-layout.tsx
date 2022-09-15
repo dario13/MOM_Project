957ca17933a3eff2d3017ff41102dcf6683a5d4d
dario <dariogabalec@gmail.com>
2022-09-15 11:43:48 -0300

    feat: added favicon

--- Frontend/src/components/layouts/page-layout/page-layout.tsx
+++ Frontend/src/components/layouts/page-layout/page-layout.tsx
@@ -3,22 +3,34 @@ import { Divider } from '@/components/atoms/divider'
 import { MainNavbar } from '@/components/organisms/main-navbar'
 import React from 'react'
 import { PageLayoutProps } from './page-layout.props'
+import Head from 'next/head'
 
 const PageLayout = (pageLayoutProps: PageLayoutProps) => {
   const { content } = pageLayoutProps
 
   return (
-    <FlexBox
-      gap="1rem"
-      paddingTop="0.5rem"
-      paddingLeft="0.8rem"
-      paddingRight="0.8rem"
-      className={'bg-base-200'}
-    >
-      <MainNavbar />
-      <Divider />
-      {content}
-    </FlexBox>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png" />
+        <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+
+      <FlexBox
+        gap="1rem"
+        paddingTop="0.5rem"
+        paddingLeft="0.8rem"
+        paddingRight="0.8rem"
+        className={'bg-base-200'}
+      >
+        <MainNavbar />
+        <Divider />
+        {content}
+      </FlexBox>
+    </>
   )
 }
 
