d3d697043b4f96a236e7fb28c18c5dd71dc5dbb0
dario <dariogabalec@gmail.com>
2022-09-29 12:20:44 -0300

    feat: added more useful hooks

---
+++ Frontend/src/hooks/use-eth-price.ts
@@ -0,0 +1,38 @@
+import { PriceFeedConsumer, PriceFeedConsumer__factory } from '@dario13/backend/typechain-types'
+import env from '@/config/env'
+import { BigNumber } from 'ethers'
+import { useCallback, useEffect, useState } from 'react'
+import { useWallet } from './use-wallet'
+
+export type useEthPriceType = {
+  ethUsdPrice: string
+}
+
+export const useEthPrice = (): useEthPriceType => {
+  const { signer } = useWallet()
+  const [ethUsdPrice, setEthUsdPrice] = useState('')
+  const priceFeedContractAddr = env.PRICE_FEED_CONTRACT_ADDRESS
+
+  const getUsdPrice = useCallback(async () => {
+    if (!signer) return
+
+    const priceFeedConsumerContract: PriceFeedConsumer = PriceFeedConsumer__factory.connect(
+      priceFeedContractAddr,
+      signer,
+    )
+
+    const [price, decimalQuantity] = await priceFeedConsumerContract.getLatestPrice()
+
+    const decimals = BigNumber.from(10).pow(decimalQuantity)
+
+    const priceFormatted = Number(price.div(decimals)).toFixed(2).toString()
+
+    setEthUsdPrice(priceFormatted)
+  }, [signer])
+
+  useEffect(() => {
+    getUsdPrice()
+  }, [getUsdPrice])
+
+  return { ethUsdPrice }
+}
