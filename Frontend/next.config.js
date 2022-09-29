f7eda8d85a29a15bb287080ab3428ae9449c4be6
dario <dariogabalec@gmail.com>
2022-09-29 12:24:33 -0300

    chore: added transpile module

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const withTM = require('next-transpile-modules')(['@dario13/backend'])
+
+module.exports = withTM({
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
   },
-}
+})
