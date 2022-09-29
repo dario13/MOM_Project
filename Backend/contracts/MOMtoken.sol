b49f45ff2add11e88dd9d68521a47d7e23a92a1a
dario <dariogabalec@gmail.com>
2022-09-29 15:48:09 -0300

    feat: added decimals method

--- Backend/contracts/MOMtoken.sol
+++ Backend/contracts/MOMtoken.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract MOMToken is ERC20 {
     address private owner;
 
-    constructor(address tokenOwner,uint256 initialSupply) ERC20("MajorOrMinor", "MOM") {
+    constructor(address tokenOwner, uint256 initialSupply) ERC20('MajorOrMinor', 'MOM') {
         _mint(tokenOwner, initialSupply);
         owner = tokenOwner;
     }
 
     function getOwner() public view returns (address) {
         return owner;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 0;
     }
 }
