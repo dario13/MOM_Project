8cbab063f65d077f4cb55beb665520c2f6a97ff7
dario <dariogabalec@gmail.com>
2022-09-29 12:31:36 -0300

    feat: isolated favicon in a component

--- Frontend/src/components/layouts/page-layout/page-layout.tsx
+++ Frontend/src/components/layouts/page-layout/page-layout.tsx
@@ -1,36 +1,26 @@
 import { FlexBox } from '@/components/atoms'
-import { Divider } from '@/components/atoms/divider'
 import { MainNavbar } from '@/components/organisms/main-navbar'
 import React from 'react'
 import { PageLayoutProps } from './page-layout.props'
-import Head from 'next/head'
 
 const PageLayout = (pageLayoutProps: PageLayoutProps) => {
   const { content } = pageLayoutProps
 
   return (
-    <>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png" />
-        <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-
-      <FlexBox
-        gap="1rem"
-        paddingTop="0.5rem"
-        paddingLeft="0.8rem"
-        paddingRight="0.8rem"
-        className={'bg-base-200'}
-      >
+    <FlexBox
+      gap="3.5rem"
+      paddingTop="1rem"
+      paddingLeft="0.8rem"
+      paddingRight="0.8rem"
+      className="bg-base-200"
+      height="100vh"
+      minHeight="100vh"
+    >
+      <FlexBox flex="0">
         <MainNavbar />
-        <Divider />
-        {content}
       </FlexBox>
-    </>
+      <FlexBox flex="1">{content}</FlexBox>
+    </FlexBox>
   )
 }
 
