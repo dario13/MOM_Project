6729b666c8af8f2a40dd866d707966ecdea8110c
dario <dariogabalec@gmail.com>
2022-09-30 17:12:45 -0300

    ci: added secret

--- Backend/hardhat.config.ts
+++ Backend/hardhat.config.ts
@@ -9,7 +9,7 @@ import * as dotenv from 'dotenv'
 dotenv.config({ path: '../.env' })
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ''
-const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ''
 const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER || '0'
 
