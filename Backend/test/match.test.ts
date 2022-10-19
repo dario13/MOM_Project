9df1751f66082353fe9744e99ada96560990d048
dario <dariogabalec@gmail.com>
2022-10-19 19:31:39 -0300

    feat: created match contract and tests

---
+++ Backend/test/match.test.ts
@@ -0,0 +1,72 @@
+import { expect } from './chai-setup'
+import { ethers, getNamedAccounts } from 'hardhat'
+
+import { smock } from '@defi-wonderland/smock'
+import { Match, Match__factory } from 'typechain-types'
+
+const setup = async () => {
+  const provider = ethers.provider
+
+  const { userA: playerA, userB: playerB } = await getNamedAccounts()
+
+  const MockedMatchFactory = await smock.mock<Match__factory>('Match')
+
+  // PlayerA will be the owner of the match
+  const MatchFactory: Match__factory = await ethers.getContractFactory('Match')
+  const cardsToWin = 3
+  const MatchContract: Match = await MatchFactory.deploy(playerA, cardsToWin)
+  const match = MatchContract.connect(provider.getSigner(playerA))
+
+  return {
+    match,
+    cardsToWin,
+    MockedMatchFactory,
+    playerA,
+    playerB,
+    provider,
+  }
+}
+
+describe('Match contract tests', () => {
+  it('when a match is created, the contract will have a variable indicating the player`s address and how many cards to guess', async () => {
+    // Given
+    const { match, playerA, cardsToWin } = await setup()
+
+    // When
+    const player = await match.player()
+    const cards = await match.cardsToWin()
+
+    // Then
+    expect(player).to.equal(playerA)
+    expect(cards).to.equal(cardsToWin)
+  })
+
+  it('only match`s player can make a bet', async () => {
+    // Given
+    const { playerB, provider, match } = await setup()
+
+    // When
+    const playerAmakesBet = await match.bet(true)
+    const playerBmakesBet = match.connect(provider.getSigner(playerB)).bet(true)
+
+    // Then
+    await expect(playerAmakesBet).to.emit(match, 'BetResult')
+    await expect(playerBmakesBet).to.be.revertedWith('OnlyPlayerCanCallThisFunction()')
+  })
+
+  it('if a match is over, the player cannot continue playing', async () => {
+    // Given
+    const { playerA, provider, MockedMatchFactory } = await setup()
+
+    const mockedMatchContract = MockedMatchFactory.connect(provider.getSigner(playerA))
+    const mockedMatch = await mockedMatchContract.deploy(playerA, 3)
+
+    await mockedMatch.setVariable('_gameOver', true)
+
+    // When
+    const playerAtriesToBet = mockedMatch.connect(provider.getSigner(playerA)).bet(true)
+
+    // Then
+    await expect(playerAtriesToBet).to.be.revertedWith('GameIsOver()')
+  })
+})
