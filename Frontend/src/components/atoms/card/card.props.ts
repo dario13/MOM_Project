213b84c54668c72ed7ec3de782366162ed5d5383
dario <dariogabalec@gmail.com>
2023-04-16 23:00:30 -0300

    refactor: removed unnecesary props

--- Frontend/src/components/atoms/card/card.props.ts
+++ Frontend/src/components/atoms/card/card.props.ts
@@ -1,9 +1,8 @@
 import { ComponentBgColors, ComponentColor, IComponentBaseProps } from '@/components/types'
 import React from 'react'
 
-export type CardProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'color'> &
-  IComponentBaseProps & {
-    children: React.ReactNode
-    color?: ComponentColor | ComponentBgColors
-    border?: boolean
-  }
+export type CardProps = IComponentBaseProps & {
+  children: React.ReactNode
+  color?: ComponentColor | ComponentBgColors
+  border?: boolean
+}
