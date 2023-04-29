942250eeece49fea7942214065da3a3033a65369
dario <dariogabalec@gmail.com>
2023-04-29 17:52:06 -0300

    test: removed unnecesary mocks

--- Frontend/src/components/organisms/exchange-card/exchange-card.test.tsx
+++ Frontend/src/components/organisms/exchange-card/exchange-card.test.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { getByRole, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { ExchangeCard } from './exchange-card'
 import userEvent from '@testing-library/user-event'
 
 import { useMediaMocked } from '@/__mocks__/hooks/use-media.mock'
 import { minimumMOMToBuyInETH, minimumMOMToSell } from './buy-and-sell-card'
-import { useWalletMocked } from '@/__mocks__/hooks/use-wallet.mock'
 import { useExchangeMocked } from '@/__mocks__/hooks/use-exchange.mock'
-import { useContractConnectionMocked } from '@/__mocks__/hooks/use-contract-connection.mock'
 import { useEthPriceMocked } from '@/__mocks__/hooks/use-eth-price.mock'
 
 const renderedComponent = () => {
@@ -15,17 +13,13 @@ const renderedComponent = () => {
 }
 
 jest.mock('@/hooks/use-exchange')
-jest.mock('@/hooks/use-wallet')
 jest.mock('@/hooks/use-wallet-balance')
-jest.mock('@/hooks/use-contract-connection')
 jest.mock('@/hooks/use-eth-price')
 
 describe('ExchangeCard', () => {
   beforeEach(() => {
     useMediaMocked({ isDesktop: true })
     useExchangeMocked()
-    useWalletMocked()
-    useContractConnectionMocked()
     useEthPriceMocked()
   })
 
@@ -42,10 +36,10 @@ describe('ExchangeCard', () => {
 
   it('if the exchange mode is buy and amount input is correct, the confirm button should be enabled', async () => {
     // Given
-    const { container, getByText } = renderedComponent()
+    const { getByText, getByTestId } = renderedComponent()
 
     // When
-    const renderedAmountInput = getByRole(container, 'textbox', { name: 'amount-to-buy' })
+    const renderedAmountInput = getByTestId('Input')
     await userEvent.type(renderedAmountInput, minimumMOMToBuyInETH.toString())
     const renderedConfirmButton = getByText('Confirm', { selector: 'button' })
 
@@ -55,12 +49,12 @@ describe('ExchangeCard', () => {
 
   it('if the exchange mode is sell and amount input is correct, the confirm button should be enabled', async () => {
     // Given
-    const { container, getByText } = renderedComponent()
+    const { getByTestId, getByText } = renderedComponent()
 
     // When
     const renderedSellModeButton = getByText('Sell', { selector: 'button' })
     await userEvent.click(renderedSellModeButton)
-    const renderedAmountInput = getByRole(container, 'textbox', { name: 'amount-to-sell' })
+    const renderedAmountInput = getByTestId('Input')
     await userEvent.type(renderedAmountInput, minimumMOMToSell.toString())
     const renderedConfirmButton = getByText('Confirm', { selector: 'button' })
 
@@ -70,10 +64,10 @@ describe('ExchangeCard', () => {
 
   it('if the exchange mode is buy and amount input is incorrect, the confirm button should be disabled', async () => {
     // Given
-    const { container, getByText } = renderedComponent()
+    const { getByTestId, getByText } = renderedComponent()
 
     // When
-    const renderedAmountInput = getByRole(container, 'textbox', { name: 'amount-to-buy' })
+    const renderedAmountInput = getByTestId('Input')
     await userEvent.type(renderedAmountInput, '0')
     const renderedConfirmButton = getByText('Confirm', { selector: 'button' })
 
@@ -83,12 +77,12 @@ describe('ExchangeCard', () => {
 
   it('if the exchange mode is sell and amount input is incorrect, the confirm button should be disabled', async () => {
     // Given
-    const { container, getByText } = renderedComponent()
+    const { getByTestId, getByText } = renderedComponent()
 
     // When
     const renderedSellModeButton = getByText('Sell', { selector: 'button' })
     await userEvent.click(renderedSellModeButton)
-    const renderedAmountInput = getByRole(container, 'textbox', { name: 'amount-to-sell' })
+    const renderedAmountInput = getByTestId('Input')
     await userEvent.type(renderedAmountInput, '0')
     const renderedConfirmButton = getByText('Confirm', { selector: 'button' })
 
