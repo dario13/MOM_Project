9b4bda182edca6fdfea7c4f339bb704fd74e2af5
dario <dariogabalec@gmail.com>
2023-04-29 17:51:14 -0300

    refactor: improved code

--- Frontend/src/hooks/use-eth-price.ts
+++ Frontend/src/hooks/use-eth-price.ts
@@ -1,32 +1,36 @@
 import { BigNumber } from 'ethers'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useWallet } from './use-wallet'
 import { useContractConnection } from './use-contract-connection'
+import { useHandleBlockchainOperations } from './use-handle-blockchain-operations'
 
 export type useEthPriceType = {
   ethUsdPrice: string
 }
 
 export const useEthPrice = (): useEthPriceType => {
-  const { signer } = useWallet()
+  const { isAccountConnected, signer } = useWallet()
   const [ethUsdPrice, setEthUsdPrice] = useState('0')
-  const { priceFeedConsumerContract } = useContractConnection()
+  const { priceFeedConsumerContract } = useContractConnection(signer)
+  const { handleCall } = useHandleBlockchainOperations()
+
+  const getUsdPrice = useCallback(async () => {
+    if (!isAccountConnected) return
+
+    const [price, decimalQuantity] = (await handleCall(
+      priceFeedConsumerContract.getLatestPrice(),
+    )) || [BigNumber.from(0), 1]
+
+    const decimals = BigNumber.from(10).pow(decimalQuantity)
+
+    const priceFormatted = Number(price.div(decimals)).toFixed(2).toString()
+
+    setEthUsdPrice(priceFormatted)
+  }, [isAccountConnected])
 
   useEffect(() => {
-    const getUsdPrice = async () => {
-      if (!signer) return '0'
-
-      const [price, decimalQuantity] = await priceFeedConsumerContract.getLatestPrice()
-
-      const decimals = BigNumber.from(10).pow(decimalQuantity)
-
-      const priceFormatted = Number(price.div(decimals)).toFixed(2).toString()
-
-      setEthUsdPrice(priceFormatted)
-    }
-
     getUsdPrice()
-  })
+  }, [getUsdPrice])
 
   return { ethUsdPrice }
 }
