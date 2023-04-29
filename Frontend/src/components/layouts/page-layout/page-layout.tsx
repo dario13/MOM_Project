2633513a2f5da70764d53764f81eb93395e53337
dario <dariogabalec@gmail.com>
2023-04-29 17:56:57 -0300

    feat: wrapped pageLayout into themeProvider

--- Frontend/src/components/layouts/page-layout/page-layout.tsx
+++ Frontend/src/components/layouts/page-layout/page-layout.tsx
@@ -2,25 +2,28 @@ import { FlexBox } from '@/components/atoms'
 import { MainNavbar } from '@/components/organisms/main-navbar'
 import React from 'react'
 import { PageLayoutProps } from './page-layout.props'
+import { ThemeProvider } from '@/contexts/theme/theme-provider'
 
 const PageLayout = (pageLayoutProps: PageLayoutProps) => {
   const { content } = pageLayoutProps
 
   return (
-    <FlexBox
-      gap="3.5rem"
-      paddingTop="1rem"
-      paddingLeft="0.8rem"
-      paddingRight="0.8rem"
-      className="bg-base-200"
-      height="100vh"
-      minHeight="100vh"
-    >
-      <FlexBox flex="0">
-        <MainNavbar />
+    <ThemeProvider>
+      <FlexBox
+        gap="3.5rem"
+        paddingTop="1rem"
+        paddingLeft="0.8rem"
+        paddingRight="0.8rem"
+        className="bg-base-200"
+        height="100vh"
+        minHeight="100vh"
+      >
+        <FlexBox flex="0">
+          <MainNavbar />
+        </FlexBox>
+        <FlexBox>{content}</FlexBox>
       </FlexBox>
-      <FlexBox flex="1">{content}</FlexBox>
-    </FlexBox>
+    </ThemeProvider>
   )
 }
 
