0008d95d657c5d627e924718e56b97bb9ee16a05
dario <dariogabalec@gmail.com>
2023-05-05 15:22:40 -0300

    fix: added missing ownership

--- Backend/contracts/MOMtokenV1.sol
+++ Backend/contracts/MOMtokenV1.sol
@@ -2,9 +2,11 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+import './Ownership.sol';
 
-contract MOMTokenV1 is ERC20Upgradeable {
+contract MOMTokenV1 is ERC20Upgradeable, Ownership {
     function initialize(address tokenOwner, uint256 initialSupply) public initializer {
+        initializeOwnership(tokenOwner);
         __ERC20_init('MajorOrMinor', 'MOM');
         _mint(tokenOwner, initialSupply);
     }
