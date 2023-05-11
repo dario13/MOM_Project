b8c7473634c4d9af5d6055c6e39ea82ee057ec99
dario <dariogabalec@gmail.com>
2023-05-11 18:01:57 -0300

    style: updated components to new home

--- Frontend/src/components/layouts/page-layout/page-layout.tsx
+++ Frontend/src/components/layouts/page-layout/page-layout.tsx
@@ -5,18 +5,42 @@ import { PageLayoutProps } from './page-layout.props'
 import { ThemeProvider } from '@/contexts/theme/theme-provider'
 
 const PageLayout = (pageLayoutProps: PageLayoutProps) => {
-  const { content } = pageLayoutProps
+  const { content, name } = pageLayoutProps
 
-  return (
-    <ThemeProvider>
-      <FlexBox gap="5rem" className="bg-base-200" height="100vh" minHeight="100vh">
-        <FlexBox maxHeight="5%" paddingTop="1rem" paddingLeft="0.8rem" paddingRight="0.8rem">
-          <MainNavbar />
+  const renderHome = () => {
+    return (
+      <ThemeProvider>
+        <FlexBox height="100vh" minHeight="100vh">
+          {content}
+          <FlexBox
+            maxHeight="5%"
+            paddingTop="1rem"
+            paddingLeft="0.8rem"
+            paddingRight="0.8rem"
+            style={{ position: 'absolute' }}
+            width="100%"
+          >
+            <MainNavbar />
+          </FlexBox>
         </FlexBox>
-        <FlexBox>{content}</FlexBox>
-      </FlexBox>
-    </ThemeProvider>
-  )
+      </ThemeProvider>
+    )
+  }
+
+  const renderDefaultLayout = () => {
+    return (
+      <ThemeProvider>
+        <FlexBox gap="5rem" className="bg-base-200">
+          <FlexBox maxHeight="5%" paddingTop="1rem" paddingLeft="0.8rem" paddingRight="0.8rem">
+            <MainNavbar />
+          </FlexBox>
+          <FlexBox>{content}</FlexBox>
+        </FlexBox>
+      </ThemeProvider>
+    )
+  }
+
+  return name === 'Home' ? renderHome() : renderDefaultLayout()
 }
 
 PageLayout.displayName = 'PageLayout'
--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { FlexBox } from '@/components'
 import { PageLayout } from '@/components/layouts/page-layout'
+import { HomeAnimated } from '@/components/molecules'
 
 const Home = () => {
   const renderHome = () => {
-    return <FlexBox alignItems="center"></FlexBox>
+    return (
+      <FlexBox>
+        <HomeAnimated />
+      </FlexBox>
+    )
   }
 
-  return <PageLayout content={renderHome()} />
+  return <PageLayout name={'Home'} content={renderHome()} />
 }
 
 export default Home
